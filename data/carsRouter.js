656f3b50beb2d11ae60b14f9e863de36fbb24266
rogerLee <rmlee0003@gmail.com>
2019-12-22 16:53:46 -0500

    created get and post endpoints

--- data/carsRouter.js
+++ data/carsRouter.js
@@ -13,4 +13,15 @@ router.get("/", async (req,res,next) => {
     }
 })
 
+router.post("/", async (req,res,next) => {
+    try {
+        const ids = await db("cars").insert(req.body)
+        const newCar = await db("cars").where({id: ids[0] }).first()
+        res.status(201).json(newCar)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
 module.exports = router
