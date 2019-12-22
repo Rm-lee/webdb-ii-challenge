0a6d4773fc6d0515b055df71f74d00b24d802550
rogerLee <rmlee0003@gmail.com>
2019-12-22 16:27:37 -0500

    create schema

--- migrations/20191222161758_cars.js
+++ migrations/20191222161758_cars.js
@@ -1,6 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("cars", (table) => {
+      table.increments("id")
+      table.text("model").notNull()
+      table.text("vin").notNull().unique()
+      table.text("make").notNull()
+      table.integer("mileage").notNull()
+      table.boolean("automatic").defaultTo(1)
+      table.text("title")
+  })
 };
 
 exports.down = function(knex) {
